9952bb60fa2569ab4291175cb7ec2209c01237e9
lbs-in-algoblu-win <1940960573@qq.com>
2017-07-14 15:17:03 +0800

    Did some change to the server.cpp.and then I will change the working environment

--- Model_CS/server.cpp
+++ Model_CS/server.cpp
@@ -1,1 +1,21 @@
 #include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <cstdio>
+#include <cstring>
+
+#define MAXBUFFERSIZE 1024
+#define MAXFILENAMESIZE 512
+
+int main(int argc, char* argv[])
+{
+
+	struct sockaddr_in server_addr;
+	memset(&server_addr,0,sizeof(server_addr));
+
+	server_addr.sin_family = AF_INET;
+
+
+
+
+}
